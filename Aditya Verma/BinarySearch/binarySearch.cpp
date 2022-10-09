efb6445c4410c60443decd23b972f9c9eec36935
Aman Kumar <amanrajkumar4@gmail.com>
2022-10-09 22:17:02 +0530

    Added Binary Search Solution

--- Aditya Verma/BinarySearch/binarySearch.cpp
+++ Aditya Verma/BinarySearch/binarySearch.cpp
@@ -1,13 +1,33 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int binarSearch(int target){
+int binarSearch(vector<int> &nums, int target){
+    int start = 0, mid = 0, end = nums.size() - 1;
+
+    while(start <= end){
+        mid = start + (end - start) / 2;
+        if(nums[mid] == target){
+            return mid;
+        }
+
+        if(nums[mid] < target){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
 
     return -1;
 }
 
 
 int main(){
+    vector<int> nums{2,4,5,7,9,12,15};
+    int target = 21;
+
+    cout << binarSearch(nums, target);
 
     return 0;
 }
