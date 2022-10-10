ddeb96e57b989e9219d645e90b86e606cb3df3ca
Aman Kumar <amanrajkumar4@gmail.com>
2022-10-10 23:19:05 +0530

    Fixed order agnostic Binary Search Solution and added binary search on descending sorted array.

--- Aditya Verma/BinarySearch/orderAgnosticBS.cpp
+++ Aditya Verma/BinarySearch/orderAgnosticBS.cpp
@@ -1,9 +1,9 @@
-// Binary Search on Descending sorted Array.
+// Binary Search on unknown orded shorted array.
 #include<iostream>
 #include<vector>
 using namespace std;
 
-int binarySearch(vector<int> &nums, int target){
+int reversebinarySearch(vector<int> &nums, int target){
     int start = 0, mid = 0, end = nums.size() - 1;
 
     while(start <= end){
@@ -21,13 +21,33 @@ int binarySearch(vector<int> &nums, int target){
     return -1;
 }
 
+int binarySearch(vector<int> &nums, int target){
+    int start = 0, mid = 0, end = nums.size() - 1;
+
+    while(start <= end){
+        mid = start + (end - start) / 2;
+
+        if(nums[mid] == target)
+            return mid;
+        
+        if(nums[mid] < target)
+            start = mid + 1;
+        else 
+            end = mid - 1;
+    }
+
+    return -1;
+}
 
 int main()
 {
-    vector<int> nums{20,17,15,13,12,11,9,5,2};
-    int target = 17;
+    vector<int> nums{1,2,3,4,5,7,8,11,15,18};
+    int target = 15, size = nums.size() - 1;
 
-    cout << binarySearch(nums, target) << endl;    
+    if(nums[0] < nums[size])
+        cout << binarySearch(nums, target) << endl;
+    else
+        cout << reversebinarySearch(nums, target) << endl;
 
     return 0;
 }
