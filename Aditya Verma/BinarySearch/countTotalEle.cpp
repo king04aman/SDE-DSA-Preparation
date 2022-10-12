c655267270510da84fd472c1ba35021e3de79008
Aman Kumar <amanrajkumar4@gmail.com>
2022-10-13 01:15:40 +0530

    Added count total number of element sloutions.

---
+++ Aditya Verma/BinarySearch/countTotalEle.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> getFirstAndLastOcc(vector<int> &nums, int target){
+    vector<int> ans(2,-1);
+    int start = 0, mid = 0, end = nums.size() - 1;
+
+    // Getting First Occurance
+    while(start <= end){
+        mid = start + (end - start) / 2;
+
+        if(nums[mid] == target){
+            ans[0] = mid;
+            end = mid - 1;
+        }
+
+        if(nums[mid] < target)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+
+    // Getting Last Occurance
+    start = 0, mid = 0, end = nums.size() - 1;
+    while(start <= end){
+        mid = start + (end - start) / 2;
+
+        if(nums[mid] == target){
+            ans[1] = mid;
+            start = mid + 1;
+        }
+
+        if(nums[mid] < target)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums{2,3,9,9,9,9,9,11,14,18};
+    int target = 9;
+
+    vector<int> temp =  getFirstAndLastOcc(nums, target);
+
+    cout << temp[1] - temp[0] << endl;
+
+    return 0;
+}
