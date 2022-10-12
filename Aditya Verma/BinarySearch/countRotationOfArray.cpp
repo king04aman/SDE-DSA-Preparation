729d790ba2a98eb5aa3354e68c2597d121318832
Aman Kumar <amanrajkumar4@gmail.com>
2022-10-12 23:38:23 +0530

    Added second method for 'Count Rotation in sorted rotated array'

--- Aditya Verma/BinarySearch/countRotationOfArray.cpp
+++ Aditya Verma/BinarySearch/countRotationOfArray.cpp
@@ -17,12 +17,31 @@ int getRotation(vector<int> &nums){
     return start;
 }
 
+// Method 2
+int getRotations(vector<int> &nums){
+    int start = 0, mid = 0, size = nums.size(), end = size - 1;
+
+    while(start <= end){
+        mid = start + (end - start) / 2;
+        int next = (mid + 1) % size;
+        int prev = (mid + size - 1) % size;
+
+        if(nums[mid] <= nums[next] && nums[mid] <= nums[prev])
+            return mid;
+
+        if(nums[start] <= nums[mid])
+            start = mid + 1;
+        else if(nums[end] <= nums[mid])
+            end = mid - 1;
+    }
+    return -1;
+}
 
 int main()
 {
-    vector<int> nums = {3,4,5,1,2};
+    vector<int> nums = {2,4,5,1,2};
 
-    cout << getRotation(nums) << endl;
+    cout << getRotations(nums) << endl;
 
     return 0;
 }
