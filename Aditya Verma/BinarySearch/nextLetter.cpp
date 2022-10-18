a6cc71a7a997ba3f8c78f0974406d88cc7851be8
Aman Kumar <amanrajkumar4@gmail.com>
2022-10-19 02:06:40 +0530

    Added program to get next alphabet fron a sorted array.

---
+++ Aditya Verma/BinarySearch/nextLetter.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int getNextLetter(vector<char> &alpha, char target){
+    int start = 0, mid = 0, end = alpha.size() - 1;
+    int ans = -1;
+
+    while(start <= end){
+        mid = start + (end - start) / 2;
+
+        if(alpha[mid] <= target){
+            start = mid + 1;
+        }
+        else if(alpha[mid] > target){
+            ans = mid;
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    vector<char> alpha {'a', 'c', 'f', 'h'};
+    char target = 'c';
+
+    cout << getNextLetter(alpha, target) << endl;
+
+    return 0;
+}
